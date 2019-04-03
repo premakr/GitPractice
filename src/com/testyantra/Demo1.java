8e4c98d654934affc6814fb3361a47eb9c42d564
srinidhi1071 <srinidhi1071@gmail.com>
2019-04-03 17:07:21 +0530

    branch commit

--- src/com/testyantra/Demo1.java
+++ src/com/testyantra/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Srinidhi");
 		System.out.println("I am from Banglore");
+		System.out.println("I am from Karnataka");
 
 	}
 
